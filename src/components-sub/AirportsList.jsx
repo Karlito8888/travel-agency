843b4abac8e7c4b2849326318378bccd23e199bf
Karlito8888 <cb.dweb49@gmail.com>
2023-12-28 20:26:29 +0800

    SelectTripType ClassSelector CityList OK

--- src/components-sub/AirportsList.jsx
+++ src/components-sub/AirportsList.jsx
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlaneUp } from "@fortawesome/free-solid-svg-icons";
-import { Input } from "antd";
-
+import AsyncSelect from "react-select/async";
 
 const AirportsList = ({ flightNumber, inputType, required }) => {
   const [allAirports, setAllAirports] = useState([]);
-  const [filteredAirports, setFilteredAirports] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
   const [inputValue, setInputValue] = useState("");
-  const [suggestionsVisible, setSuggestionsVisible] = useState(true);
+  const [inputLength, setInputLength] = useState(0);
 
   useEffect(() => {
     axios
@@ -27,50 +22,28 @@ const AirportsList = ({ flightNumber, inputType, required }) => {
       .catch((error) => console.error("Error fetching data", error));
   }, []);
 
-  useEffect(() => {
-    if (searchTerm.length >= 3) {
-      const filtered = allAirports.filter((airport) => {
-        const searchRegex = new RegExp(searchTerm, "i");
-        return (
-          searchRegex.test(airport.city) ||
-          searchRegex.test(airport.name) ||
-          searchRegex.test(airport.iata_code) ||
-          searchRegex.test(airport.country) ||
-          airport.city.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
-          airport.name.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
-          airport.iata_code
-            .toLowerCase()
-            .startsWith(searchTerm.toLowerCase()) ||
-          airport.country.toLowerCase().startsWith(searchTerm.toLowerCase())
-        );
-      });
-      setFilteredAirports(filtered);
-      setSuggestionsVisible(true);
+  const filteredAirports = useMemo(() => {
+    return allAirports.filter(
+      (airport) =>
+        airport.city.toLowerCase().includes(inputValue.toLowerCase()) ||
+        airport.name.toLowerCase().includes(inputValue.toLowerCase()) ||
+        airport.iata_code.toLowerCase().includes(inputValue.toLowerCase()) ||
+        airport.country.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  }, [inputValue, allAirports]);
+
+  const handleChange = (selectedOption) => {
+    if (selectedOption) {
+      const { city, name, iata_code, country } = selectedOption;
+
+      setInputValue(`${city}, ${name} ${iata_code}`);
+
+      localStorage.setItem(`${inputType}City${flightNumber}`, city);
+      localStorage.setItem(`${inputType}Name${flightNumber}`, name);
+      localStorage.setItem(`${inputType}IataCode${flightNumber}`, iata_code);
+      localStorage.setItem(`${inputType}Country${flightNumber}`, country);
     } else {
-      setFilteredAirports(allAirports);
-      setSuggestionsVisible(false);
-    }
-  }, [searchTerm, allAirports]);
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-    }
-    if (e.key === "Delete" || e.key === "Backspace") {
-        setInputValue("");
-        setSuggestionsVisible(false);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    const value = e.target.value;
-    setSearchTerm(value);
-    setInputValue(value);
-
-    if (value.length >= 3) {
-      setSuggestionsVisible(true);
-    } else {
-      setSuggestionsVisible(false);
+      setInputValue("");
       localStorage.removeItem(`${inputType}City${flightNumber}`);
       localStorage.removeItem(`${inputType}Name${flightNumber}`);
       localStorage.removeItem(`${inputType}IataCode${flightNumber}`);
@@ -78,57 +51,67 @@ const AirportsList = ({ flightNumber, inputType, required }) => {
     }
   };
 
-  const handleSuggestionClick = (airportData) => {
-    const { city, name, iata_code, country } = airportData;
-    
-    setInputValue(`${city}, ${name} ${iata_code}`);
-    setSuggestionsVisible(false);
+  const loadOptions = (searchValue, callback) => {
+    if (searchValue.length >= 2) {
+      const filteredOptions = allAirports.filter(
+        (airport) =>
+          airport.city.toLowerCase().includes(searchValue.toLowerCase()) ||
+          airport.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+          airport.iata_code.toLowerCase().includes(searchValue.toLowerCase()) ||
+          airport.country.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      callback(filteredOptions);
+    } else {
+      callback([]);
+    }
+  };
 
-    localStorage.setItem(`${inputType}City${flightNumber}`, city);
-    localStorage.setItem(`${inputType}Name${flightNumber}`, name);
-    localStorage.setItem(`${inputType}IataCode${flightNumber}`, iata_code);
-    localStorage.setItem(`${inputType}Country${flightNumber}`, country);
+  const handleInputChange = (newValue) => {
+    setInputValue(newValue);
+    setInputLength(newValue.length);
+  };
+
+  const customStyles = {
+    control: (provided, state) => ({
+      // provided conserve les styles de bases tout en ajoutant ou modifiant ceux qu'on souhaite personnaliser.
+      // state permet de conditionner les styles en fonction de l'état de l'élément... state.isFocused, etc...
+      ...provided,
+      fontSize: "0.8rem",
+      minWidth: "192px",
+      marginBottom: "7px",
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      fontSize: "0.7rem",
+      textAlign: "left",
+      position: "relative",
+      top: "20px",
+    }),
+    menu: (provided, state) => ({
+      ...provided,
+      display: inputLength >= 2 ? "block" : "none",
+    }),
   };
 
   return (
-    <React.Fragment>
-      <Input
-        type="text"
-        id={`${inputType}-city-${flightNumber}`}
-        name={`${inputType}-city`}
-        placeholder={`Ville de ${
-          inputType === "departure" ? "départ" : "destination"
-        }`}
-        value={inputValue}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-        autoComplete="off"
-        allowClear
-        required
-      />
-      {searchTerm.length >= 3 && suggestionsVisible && (
-        <div className="suggestions">
-          {filteredAirports.length > 0 ? (
-            <ul>
-              {filteredAirports &&
-                filteredAirports.slice(0, 3).map((airportData, index) => (
-                  <li
-                    key={index}
-                    onClick={() => handleSuggestionClick(airportData)}
-                  >
-                    <FontAwesomeIcon icon={faPlaneUp} />
-                    &nbsp;{airportData.city},&nbsp;{airportData.name}
-                    &nbsp;&#x2768;
-                    {airportData.iata_code}&#x2769;&nbsp;-&nbsp;{airportData.country}
-                  </li>
-                ))}
-            </ul>
-          ) : (
-            <p>Aucune suggestion trouvée</p>
-          )}
-        </div>
-      )}
-    </React.Fragment>
+    <AsyncSelect
+      styles={customStyles}
+      menuPlacement={"bottom"}
+      maxMenuHeight={"120px"}
+      menuWidth={"300px"}
+      cacheOptions
+      loadOptions={loadOptions}
+      onChange={handleChange}
+      isClearable={true}
+      getOptionLabel={(airport) =>
+        `${airport.city}, ${airport.name} \u2768${airport.iata_code}\u2769 - ${airport.country}`
+      }
+      getOptionValue={(airport) => airport.iata_code}
+      placeholder={`Ville de ${
+        inputType === "departure" ? "départ" : "destination"
+      } `}
+      onInputChange={handleInputChange}
+    />
   );
 };
 
